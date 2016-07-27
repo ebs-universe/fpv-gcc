e415c4d0088e9eb177f78cbb021b6915c295db91
Chintalagiri Shashank <shashank@chintal.in>
2016-07-28 02:27:27 +0530

    Fixed sphinx conf.

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath(os.pardir))
 autodoc_default_flags = ['members', 'undoc-members', 'private-members', 'show-inheritance']
 autodoc_member_order = 'bysource'
 autoclass_content = 'init'
